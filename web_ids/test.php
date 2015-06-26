08f705e7da7f8e732ce3b4b643996cf7160e551b
IvyHesn <hsnstudy@126.com>
2015-06-26 14:27:48 +0800

    实现切换

---
+++ web_ids/test.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<script type="text/javascript" language="php">
+	</script>
+</head>
+<body>
+<?php
+$a = 1;
+?>
+<p>$a = <?php echo $a;?></p>
+<?php
+$a++;
+?>
+<p>$a = <?php echo $a;?></p>
+</body>
+</html>
+
